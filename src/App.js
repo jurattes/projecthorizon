5542e90d21b56dd30d82337256e90cccf2feb5b9
Justin Pescador <heartfullycried@gmail.com>
2024-09-17 16:46:20 -0400

    Added intial Headers + Updated App JS/CSS

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Registration from './Components/Registration';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <BrowserRouter>
     <div className="App">
       <header className="App-header">
-        <Routes>
-          <Route path = "/register">
-          <Registration />
-          </Route>
-        </Routes>
+        <Header />
       </header>
+      <h1> AAAAAAAAAAAAAAAAAAA </h1>
     </div>
-    </BrowserRouter>
   );
 }
 
