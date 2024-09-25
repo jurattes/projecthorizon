c8ba7b5721944baaf94b78d66750bd6b9c890e64
Justin Pescador <j.pescador03@gmail.com>
2024-09-25 02:00:55 -0400

    Added React Router + Login (WIP)

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import './App.css';
 import Registration from './Components/Registration';
 import Header from './Components/Header';
 import UserPool from './Components/UserPool';
+import Login from './Components/Login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <header className="App-header">
         <Header />
       </header>
-      <Registration />
-      <h1> AAAAAAAAAAAAAAAAAAA </h1>
+      <div className = "content">
+      <Switch>
+        <Route path = "/register">
+          <Registration />
+        </Route>
+        <Route path = "/login">
+          <Login />
+        </Route>
+      </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
