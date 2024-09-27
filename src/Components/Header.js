5b275d6f972557fa9b0a0aebeb4dbead1c20272b
Justin Pescador <heartfullycried@gmail.com>
2024-09-27 15:39:53 -0400

    Updated header to match session

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,38 @@
-import React from 'react';
 import './Header.css';
+import settings from './assets/settings.svg';
+import React, { useEffect, useContext, useState } from 'react';
+import { AccountSettingsContext } from './AccountSettings';
 
 function Header() {
+  const { getSession } = useContext(AccountSettingsContext);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        getSession()
+            .then((data) => {
+                console.log(data);
+                setLoggedIn(true);
+            })
+            .catch((err) => {
+                console.error(err);
+                setLoggedIn(false);
+            });
+    }, []);
+
   return (
     <header>
       <nav>
         <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-          <li><a href="#search">Search</a></li>
+          <li><a href="/">Home</a></li>
+          {!loggedIn && (
+                <li><a href="/login">Login</a></li>
+            )}
+          {!loggedIn && (
+                <li><a href="/register">Register</a></li>
+            )}
+          {loggedIn && (
+                <li><a href="/settings"><img src={settings} alt="Settings" /></a></li>
+            )}
         </ul>
       </nav>
     </header>
